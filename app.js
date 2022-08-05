9c3eacf025c241939c919d98176efc2f66e8b5c8
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-08-05 08:36:41 -0500

    almost done

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
 
-
-    
-
-
-const boxes = document.querySelectorAll(".box");
 const playerX = "X";
 const playerO = "O";
-let turn = playerX;
-
-
- let gameState = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-
-const lineThru = document.getElementsByClassName("lineThru");
+const boxes = document.querySelectorAll(".box");
 const gameOver = document.getElementById("gameOver");
 const gameOverText = document.getElementById("gameOverText");
 const playAgain = document.getElementById("playAgain");
+const playerXinput = document.getElementById("playerXinput")
+const playerXlabel = document.getElementById("playerXlabel")
+const playerOinput = document.getElementById("playerOinput")
+const playerOlabel = document.getElementById("playerOlabel")
+const playerXinputform = document.getElementById("playerXinputform")
+const playerOinputform = document.getElementById("playerOinputform")
+
+playerXinputform.addEventListener("submit", (event)=>{
+event.preventDefault();
+const playerXName = event.target[0]
+playerXlabel.innerHTML = playerXName.value
+})
+playerOinputform.addEventListener("submit", (event)=>{
+  event.preventDefault();
+  const playerOName = event.target[0]
+  playerOlabel.innerHTML = playerOName.value
+  })
+
+
+let turn = playerX;
+let gameState = [null, null, null,
+  null, null, null,
+  null, null, null ]
+restartGame()
+
+
+
+
+function restartGame(){
+  if(playAgain){ gameState = [null, null, null,
+  null, null, null,
+  null, null, null ]
+
+
+  boxes.forEach((box)=>box.innerHTML="");
+  boxes.forEach((box)=>box.removeEventListener("click", boxClick));
+  boxes.forEach((box)=>box.addEventListener("click", boxClick));
+ 
+  
+  gameOver.className = "hidden";
+
+
+  console.log(gameState, "hahahahahah")
+}}
+
+
+const lineThru = document.getElementsByClassName("lineThru");
+
 
 
 boxes.forEach((box)=>box.addEventListener("click", boxClick));
@@ -27,22 +60,25 @@ boxes.forEach((box)=>box.addEventListener("click", boxClick));
 
 
 function boxClick(event){
+  console.log(event.target.id," clicky")
   if(gameOver.classList.contains('visible')){
     return;
   }
   const box = event.target;
-  const boxNum = box.dataset.index;
+  const id = event.target.id
   if(box.innerText != ""){
     return;
   }
   if(turn === playerX){
     box.innerText = playerX;
-    gameState[boxNum - 1] = playerX;
+    gameState[id - 1] = playerX;
+    console.log(gameState, "gamestate")
     turn = playerO;
   }
   else{
     box.innerText = playerO;
-    gameState[boxNum - 1] = playerO;
+   gameState[id - 1] = playerO;
+   console.log(gameState, "gamestate")
     turn = playerX;
   }
   
@@ -51,24 +87,29 @@ function boxClick(event){
 
 
 function checkFTW(){
+  if(gameState != [
+    null, null, null,
+    null, null, null,
+  null, null, null
+  ]){
   for(const winningCondition of winningConditions){
-  const {combo, lineThruClass} = winningCondition;
+  const {combo} = winningCondition;
     const boxValue1 = gameState[combo[0] - 1];
     const boxValue2 = gameState[combo[1] - 1];
     const boxValue3 = gameState[combo[2] - 1];
 
 
     if(boxValue1 != null && boxValue1 === boxValue2 && boxValue1 === boxValue3){
-      lineThru.classList.add(lineThruClass);
+      // lineThru.classList.push(lineThruClass);
       gameOverScreen(boxValue1);
       return;     
     }
   }
   const tieGame= (gameState != 
-    [null, null, null],
-    [null, null, null],
-    [null, null, null] )
-    }
+    null, null, null,
+    null, null, null,
+    null, null, null )
+    }}
 
 
 function gameOverScreen(winnerText){
@@ -84,14 +125,16 @@ gameOverText.innerText = text;
 
 
 const winningConditions  = [
-  {combo:[1,2,3], lineThruClass: "lineThruRow1"},
-  {combo:[4,5,6], lineThruClass: "lineThruRow2"},
-  {combo:[7,8,9], lineThruClass: "lineThruRow3"},
-  {combo:[1,4,7], lineThruClass: "lineThruCol1"},
-  {combo:[2,5,8], lineThruClass: "lineThruCol2"},
-  {combo:[3,6,9], lineThruClass: "lineThruCol3"},
-  {combo:[1,5,9], lineThruClass: "lineThruDiag1"},
-  {combo:[3,5,7], lineThruClass: "lineThruDiag2"},
+  {combo:[1,2,3]},
+  {combo:[4,5,6]},
+  {combo:[7,8,9]},
+  {combo:[1,4,7]},
+  {combo:[2,5,8]},
+  {combo:[3,6,9]},
+  {combo:[1,5,9]},
+  {combo:[3,5,7]},
 ]
 
+
+playAgain.addEventListener("click", restartGame)
 
